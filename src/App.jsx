32e8b79625a69985d685f43a781cea58b2e9238d
catmiyc <catmiyc@163.com>
2023-06-01 18:30:47 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './Hero';
+import Projects from './Projects';
+const App = () => {
+  return (
+    <main>
+      <Hero></Hero>
+      <Projects></Projects>
+    </main>
+  );
+};
+export default App;
---
+++ src/fetchProjects.jsx
@@ -0,0 +1,41 @@
+import { createClient } from 'contentful';
+import { useEffect, useState } from 'react';
+
+const client = createClient({
+  space: 'fye55lse1n75',
+  environment: 'master',
+  accessToken: 'wkjVWQoh8RQrCFaM8ZHKq_WQgzXI9AcAAatMl2jHZDs',
+});
+
+const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await client.getEntries({ content_type: 'projects' });
+      const projects = res.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return { loading, projects };
+};
+
+// client
+//   .getEntries({ content_type: 'projects' })
+//   .then((response) => console.log(response.items))
+//   .catch(console.error);
+
+export default useFetchProjects;
