8ba7ed03fd26afebe437ad8a6b91ac58eb6fea01
catmiyc <catmiyc@163.com>
2023-06-08 13:33:18 +0800

    change hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,10 +6,7 @@ const Hero = () => {
       <div className="hero-center">
         <div className="hero-title">
           <h1>知行合一</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
-            aliquam!
-          </p>
+          <p>知止而后有定；定而后能静；静而后能安；安而后能虑；虑而后能得</p>
         </div>
         <div className="img-container">
           <img src={heroImg} alt="hero img" className="img" />
